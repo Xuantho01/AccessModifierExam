352957b0befdb0ea7fcca461827a5597343dc11f
Xuantho01 <hoxuantho95@gmail.com>
2020-02-18 15:53:51 +0700

    accessmodifier exam

---
+++ src/pack1/Acessmodi.java
@@ -0,0 +1,7 @@
+package pack1;
+
+public class Acessmodi {
+        protected void msg(){System.out.println("Hello");}
+}
+
+
---
+++ src/pack2/Protected.java
@@ -0,0 +1,8 @@
+package pack2;
+import pack1.*;
+public class Protected extends Acessmodi {
+    public static void main(String args[]){
+        Protected obj = new Protected();
+        obj.msg();
+    }
+}
